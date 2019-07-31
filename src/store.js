6826e75405b235ef259b5de0aa226cd7d13e1ae1
BiggaHD <dzenis.hankusic@gmail.com>
2019-07-31 22:15:02 +0200

    delete firebase config data

--- src/store.js
+++ src/store.js
@@ -8,13 +8,14 @@ import { reduxFirestore, firestoreReducer } from "redux-firestore";
 // import settingsReducer from "./reducers/settingsReducer";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjVkTaWYsYlxkRXlaqlPdW9-H65Fr3l5I",
-  authDomain: "reactive-rss-d3f9c.firebaseapp.com",
-  databaseURL: "https://reactive-rss-d3f9c.firebaseio.com",
-  projectId: "reactive-rss-d3f9c",
-  storageBucket: "reactive-rss-d3f9c.appspot.com",
-  messagingSenderId: "391343765670",
-  appId: "1:391343765670:web:299051682bf16352"
+  //  YOUR FIREBASE CONFIG FILE GOES HERE ...
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // react-redux-firebase config
